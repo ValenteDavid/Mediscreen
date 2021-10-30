ebc551f9670e81ea88182e5bbd95135d92c81010
David Valente <dvalente77@gmail.com>
2021-10-30 23:32:55 +0200

    Update schema.sql

--- patient/src/main/resources/schema.sql
+++ patient/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE patient (
-    idPatient INT AUTO_INCREMENT NOT NULL,
-    firstName VARCHAR(200) NOT NULL,
-    lastName VARCHAR(200) NOT NULL,
+CREATE TABLE IF NOT EXISTS patient (
+    patient_id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
     dob DATE NOT NULL,
     sex CHAR(1) NOT NULL,
     address VARCHAR(500),
     phone VARCHAR(12),
-    PRIMARY KEY (idPatient),
-    UNIQUE (firstName , lastName)
+    PRIMARY KEY (patient_id),
+    UNIQUE (first_name , last_name)
 );
