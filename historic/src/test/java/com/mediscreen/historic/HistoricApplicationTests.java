63958f2811e21e1f9f54fad030c010ce0cccbcd6
David Valente <dvalente77@gmail.com>
2021-11-11 14:38:02 +0100

    Create MS Historic

---
+++ historic/src/test/java/com/mediscreen/historic/HistoricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.historic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
